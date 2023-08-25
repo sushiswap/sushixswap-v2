f4e4a5c69e0f0f001cbdf28fc779aa74705f3cc0
0xJiro <jiro@sushi.com>
2023-08-25 11:21:21 +0800

    added CCIPAdapter skeleton

---
+++ src/adapters/CCIPAdapter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.10;
+
+import "../interfaces/ISushiXSwapV2Adapter.sol";
+import "../interfaces/IRouteProcessor.sol";
+import "../interfaces/IWETH.sol";
+
+
+
+contract CCIPAdapter is ISushiXSwapV2Adapter {
+
+  
+
+
+}
