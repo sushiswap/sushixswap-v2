56e44f4aca1223adca6a6982c3e760a7bcee1407
0xMasayoshi <0xmasayoshi@protonmail.com>
2023-09-19 13:50:24 +0700

    feat: setup forge-deploy

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ install: init
 init:
 	git submodule update --init --recursive
 	forge install
+	cd lib/forge-deploy && cargo build --release && cp target/release/forge-deploy ../../forge-deploy
 test:
 	forge test -vv
 test-gas-report:
@@ -19,5 +20,7 @@ coverage:
 coverage-info:
 	forge coverage --report debug
 deploy:
-	forge script ./script/DeploySushiXSwapV2.s.sol --broadcast --slow --optimize --optimizer-runs 999999 --names --verify --rpc-url ${RPC_URL} --chain ${CHAIN_ID} --etherscan-api-key ${ETHERSCAN_API_KEY}
+	./forge-deploy gen-deployer
+	forge script ./script/DeploySushiXSwapV2.s.sol --broadcast --slow --optimize --optimizer-runs 999999 --names --verify --rpc-url ${RPC_URL} --chain ${CHAIN_ID} --etherscan-api-key ${ETHERSCAN_API_KEY} --private-key ${PRIVATE_KEY}
+	./forge-deploy sync
 .PHONY: test
--- script/DeploySushiXSwapV2.s.sol
+++ script/DeploySushiXSwapV2.s.sol
@@ -1,38 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
-import "forge-std/Script.sol";
+import "forge-deploy/DeployScript.sol";
+import {DeployerFunctions, DeployOptions} from "generated/deployer/DeployerFunctions.g.sol";
+
 import "../src/SushiXSwapV2.sol";
 import "../src/adapters/StargateAdapter.sol";
-
 import "../src/interfaces/IRouteProcessor.sol";
 
-contract DeploySushiXSwapV2 is Script {
+contract DeploySushiXSwapV2 is DeployScript {
+  using DeployerFunctions for Deployer;
+
   address _owner = vm.envAddress("OWNER_ADDRESS");
   //address _operator = vm.envAddress("OPERATOR_ADDRESS");
 
-  function run() external {
+  function deploy() external returns (SushiXSwapV2) {
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-    vm.startBroadcast(deployerPrivateKey);
 
     // deploy sushiXswapv2
     address routeProcesser = vm.envAddress("ROUTE_PROCESSOR_ADDRESS");
     address weth = vm.envAddress("WETH_ADDRESS");
-
-    SushiXSwapV2 sushiXSwap = new SushiXSwapV2(IRouteProcessor(routeProcesser), weth);
+    SushiXSwapV2 sushiXSwap = deployer.deploy_SushiXSwapV2("SushiXSwapV2", IRouteProcessor(routeProcesser), weth);
     
     // deploy stargate adapter
     address stargateRouter = vm.envAddress("STARGATE_ROUTER_ADDRESS");
     address stargateWidget = vm.envAddress("STARGATE_WIDGET_ADDRESS");
     address sgETH = vm.envAddress("SG_ETH_ADDRESS");
-    StargateAdapter stargateAdapter = new StargateAdapter(stargateRouter, stargateWidget, sgETH, routeProcesser, weth);
+    StargateAdapter stargateAdapter = deployer.deploy_StargateAdapter("StargateAdapter", stargateRouter, stargateWidget, sgETH, routeProcesser, weth);
+
+    vm.startBroadcast(deployerPrivateKey);
 
     // attach stargate & squid adapter
     sushiXSwap.updateAdapterStatus(address(stargateAdapter), true);
 
     // transfer ownership to owner
-    // set operators
     sushiXSwap.transferOwnership(_owner);
+
+    // set operators
     //sushiXSwap.setOperator(_operator);
   }
 }
